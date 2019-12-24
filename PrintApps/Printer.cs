daacf2645be355153470f6799d55728187ae8d56
ndean73 <Dean.Nalumpa@mlnr.gov.zm>
2019-12-24 01:01:38 -0800

    My First Commit

---
+++ PrintApps/Printer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintApps
+{
+
+
+    public interface IPrinter
+    {
+
+
+        public void print(String input);
+    
+    
+    };
+
+    public class Printer:IPrinter
+    {
+
+        public void print(String input) {
+
+            
+        }
+
+        public void print(String[] input) {
+
+            foreach (string line in input)
+            {
+                Utilities.Print(line);
+            }
+
+
+        }
+
+        
+    };
+
+
+    public static class Utilities{
+
+
+        public static void Print(String input)
+    {
+
+        Console.WriteLine(input);
+
+    }
+
+}
+
+
+
+}
---
+++ PrintApps/PrinterTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace PrintApps
+{
+    public class PrinterTest
+    {
+
+        private readonly StringWriter Sw = new StringWriter();
+        private readonly String expected = "Hello World";
+
+        public void SetOut()
+        {
+
+            Console.SetOut(Sw);
+
+        }
+
+        public void PrintTest() {
+
+            //Arrange
+           
+             Printer printer = new Printer();
+            
+            //Act
+            printer.print(expected);
+
+            //Assert
+
+            Assert.AreEqual(expected, Sw.ToString().Trim());
+
+        }
+
+
+
+
+        public void PrintTest2()
+        {
+
+            //Arrange
+
+            Printer printer = new Printer();
+            String[] expectedArray = { "Hello","World" };
+            //Act
+
+
+            printer.print(expectedArray);
+
+            //Assert
+
+            Assert.AreEqual(expectedArray, Sw.ToString().Trim());
+
+        }
+
+
+
+    }
+}
